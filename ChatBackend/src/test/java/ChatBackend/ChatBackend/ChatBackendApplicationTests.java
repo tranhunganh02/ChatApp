1960a381d9b4f39e9b22dd328eb129172edb4f0b
tranhunganh02 <hunganh.dh2021@gmail.com>
2024-10-15 23:52:11 +0700

    config project client

---
+++ ChatBackend/src/test/java/ChatBackend/ChatBackend/ChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ChatBackend.ChatBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
