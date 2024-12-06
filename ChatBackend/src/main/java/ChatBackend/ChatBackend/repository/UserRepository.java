d503e1528935ac2b95e49c7f537d8ee8e7f380ff
Mai Phương Nam <nammp.22it@vku.udn.vn>
2024-12-06 13:25:09 +0700

    1

--- ChatBackend/src/main/java/ChatBackend/ChatBackend/repository/UserRepository.java
+++ ChatBackend/src/main/java/ChatBackend/ChatBackend/repository/UserRepository.java
@@ -3,19 +3,24 @@ package ChatBackend.ChatBackend.repository;
 import ChatBackend.ChatBackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-  Optional<User> findByEmail(String email);
-  Boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 
-  @Query("SELECT u.id\n" +
-          "FROM User u\n" +
-          "JOIN Chat c ON u MEMBER OF c.members\n" +
-          "JOIN Message m ON c.id = m.chat.id\n" +
-          "WHERE m.id = :messageId\n" +
-          "  AND u.id != :senderId\n")
-  List<Integer> findRecipientIdByMessageIdAndSenderId(Integer messageId, Integer senderId);
+    Boolean existsByEmail(String email);
+
+    @Query("SELECT u.id\n" +
+            "FROM User u\n" +
+            "JOIN Chat c ON u MEMBER OF c.members\n" +
+            "JOIN Message m ON c.id = m.chat.id\n" +
+            "WHERE m.id = :messageId\n" +
+            "  AND u.id != :senderId\n")
+    List<Integer> findRecipientIdByMessageIdAndSenderId(Integer messageId, Integer senderId);
+
+    @Query("SELECT u FROM User u WHERE u.name LIKE :pattern")
+    List<User> searchUserByName(@Param("pattern") String pattern);
 }
--- ChatClient/app/(tabs)/search.tsx
+++ ChatClient/app/(tabs)/search.tsx
@@ -1,12 +1,127 @@
 // app/home/search.tsx
-import { useRouter } from 'expo-router';
-import { View, Text } from 'react-native';
+import searchApi, { UserSearch } from '@/apis/searchApi';
+import { UserList } from '@/components';
+import { authSelector, AuthState } from '@/state/reducers/authReducer';
+import { Link, useRouter } from 'expo-router';
+import { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity, TextInput, StyleSheet, Image } from 'react-native';
+import { useSelector } from 'react-redux';
 
 export default function Search() {
   const router = useRouter()
+  const [searchText, setSearchText] = useState('');
+  const [results, setResults] = useState<UserSearch[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [allUsers, setAllUsers] = useState<any[]>([]); 
+  const auth: AuthState = useSelector(authSelector);
+  
+  const handleSearch = async () => {
+    if (!searchText.trim()) {
+      return; 
+    }
+
+    setLoading(true);
+    setError(null);
+
+    try {
+      const accessToken = auth.accessToken;
+      console.log(accessToken)
+  
+      const data = await searchApi.searchUsersByName(searchText, accessToken);
+
+
+      setResults(data);
+      console.log(results)
+    } catch (err: any) {
+      setError(err.message || 'Something went wrong');
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Search Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.searchBar}>
+        <TextInput
+          style={styles.input}
+          value={searchText}
+          onChangeText={setSearchText}
+          placeholder="Search..."
+        />
+        <TouchableOpacity style={styles.searchButton} onPress={handleSearch}>
+          <Text style={styles.searchButtonText}>Search</Text>
+        </TouchableOpacity>
+      </View>
+      <UserList userList={results.length > 0 ? results : allUsers} />
+      <FlatList
+        data={results}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Link  href={{ pathname: "/message/[id]", params: { id: item.id,  username: item.username, image: item.avatar }}}>
+          <View style={styles.resultItem}>
+            <Text>{item.username}</Text>
+            <Text>{item.email}</Text>
+            <Text>{item.avatar}</Text>
+          </View>
+          </Link>
+        )}
+      />
+      
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#fff',
+  },
+  searchBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    paddingBottom: 8,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    paddingLeft: 10,
+  },
+  searchButton: {
+    marginLeft: 10,
+    padding: 10,
+    backgroundColor: '#4CAF50',
+    borderRadius: 8,
+  },
+  searchButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  resultItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f1f1f1',
+  },
+  userContainer: {
+    alignItems: 'center',
+    marginRight: 20
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginBottom:8
+  },
+  userName: {
+    fontSize: 14,
+    color: "white",
+    textAlign: 'center',
+    textDecorationLine: 'none',
+  },
+});
